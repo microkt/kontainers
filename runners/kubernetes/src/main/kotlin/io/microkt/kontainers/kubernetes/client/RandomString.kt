8f9b0ce0e810a52f1891a79cca9fe1e551194e3a
Scott Rossillo <scott@rossillo.net>
2022-03-01 19:25:58 -0500

    Mark random string internal

--- runners/kubernetes/src/main/kotlin/io/microkt/kontainers/kubernetes/client/RandomString.kt
+++ runners/kubernetes/src/main/kotlin/io/microkt/kontainers/kubernetes/client/RandomString.kt
@@ -3,7 +3,7 @@ package io.microkt.kontainers.kubernetes.client
 private const val STRING_LENGTH = 10
 private val charPool: List<Char> = ('a'..'z') + ('0'..'9')
 
-fun randomString() = (1..STRING_LENGTH)
+internal fun randomString() = (1..STRING_LENGTH)
     .map { kotlin.random.Random.nextInt(0, charPool.size) }
     .map(charPool::get)
     .joinToString("")
