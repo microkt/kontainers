18d23f647afdc81e1ef89d50f29a9b33364608b8
Scott Rossillo <scott@rossillo.net>
2022-03-08 10:19:08 -0500

    Add Flyway MySQL migration test

--- kontainers/mysql/build.gradle.kts
+++ kontainers/mysql/build.gradle.kts
@@ -7,6 +7,8 @@ dependencies {
     api(project(":kontainers-runner-factory"))
 
     testImplementation(project(":kontainers-junit5"))
+    testImplementation(project(":logging"))
     testImplementation("mysql:mysql-connector-java:8.0.28")
     testImplementation("com.zaxxer:HikariCP:4.0.3")
+    testImplementation("org.flywaydb:flyway-mysql:8.5.1")
 }
---
+++ kontainers/mysql/src/test/kotlin/io/microkt/kontainers/mysql/FlywayMysqlKontainerTest.kt
@@ -0,0 +1,37 @@
+package io.microkt.kontainers.mysql
+
+import io.microkt.kontainers.junit5.annotation.Kontainers
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Tags
+import org.junit.jupiter.api.Test
+import java.sql.Statement
+import javax.sql.DataSource
+
+@Kontainers
+@Tags(
+    Tag("docker"),
+    Tag("kubernetes")
+)
+internal class FlywayMysqlKontainerTest(private val mysql: MysqlKontainer) {
+    private var dataSource: DataSource = buildHikariDataSource(mysql)
+
+    init {
+        val flyway = Flyway.configure().dataSource(dataSource).load()
+        flyway.migrate()
+    }
+
+    @Test
+    fun testQueryAnimalsTable() {
+        val statement: Statement = dataSource.connection.createStatement()
+        statement.execute("SELECT count(*) from animals")
+
+        val resultSet = statement.resultSet
+        resultSet.next()
+
+        val resultInt = resultSet.getInt(1)
+        assertEquals(6, resultInt, "select count should return 6")
+    }
+}
