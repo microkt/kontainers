0b91fb5703a58e93c81c6d5007c43e5666dd7baf
Scott Rossillo <scott@rossillo.net>
2022-03-09 17:36:14 -0500

    localstack docs

--- kontainers/localstack/build.gradle.kts
+++ kontainers/localstack/build.gradle.kts
@@ -9,3 +9,16 @@ dependencies {
     testImplementation(project(":kontainers-junit5"))
     testImplementation(project(":logging"))
 }
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>().configureEach {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("LocalStack Kontainer")
+            includes.from("MODULES.md")
+            samples.from(
+                "$projectDir/src/main/kotlin",
+                "$projectDir/src/test/kotlin"
+            )
+        }
+    }
+}
--- kontainers/localstack/src/main/kotlin/io/microkt/kontainers/localstack/LocalStackKontainerSpec.kt
+++ kontainers/localstack/src/main/kotlin/io/microkt/kontainers/localstack/LocalStackKontainerSpec.kt
@@ -6,11 +6,22 @@ import io.microkt.kontainers.dsl.kontainerSpec
  * [KontainerSpec] for a [LocalStackKontainer].
  *
  * @author Scott Rossillo
+ * @sample [io.microkt.kontainers.localstack.localStackKontainerSpec]
  */
 val localStackKontainerSpec = kontainerSpec {
     name = "localstack"
-    image = "${LocalStackKontainer.Default.IMAGE}:${LocalStackKontainer.Default.VERSION}"
+    image = "localstack/localstack:0.14"
     ports {
-        expose tcp LocalStackKontainer.Default.PORT
+        expose tcp 4566
     }
+}
+
+/**
+ * [KontainerSpec] for a [LocalStackKontainer] light.
+ *
+ * @author Scott Rossillo
+ * @sample [io.microkt.kontainers.localstack.localStackLightKontainerSpec]
+ */
+val localStackLightKontainerSpec = kontainerSpec(localStackKontainerSpec) {
+    image = "localstack/localstack-light:0.14"
 }
