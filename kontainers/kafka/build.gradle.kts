24097b089293ed464f34638d2646c324574911aa
Scott Rossillo <scott@rossillo.net>
2022-03-09 17:30:56 -0500

    Exclude Kafka Kontainers

--- kontainers/kafka/build.gradle.kts
+++ kontainers/kafka/build.gradle.kts
@@ -10,3 +10,10 @@ dependencies {
     testImplementation(project(":kontainers-junit5"))
     testImplementation("org.apache.kafka:kafka-clients:2.8.1")
 }
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>().configureEach {
+    dokkaSourceSets {
+        configureEach {
+            suppress.set(true)
+        }
+    }
+}
