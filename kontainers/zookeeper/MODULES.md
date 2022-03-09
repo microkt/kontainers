cde20e006a78260c92a02800ce010d4c6d037af9
Scott Rossillo <scott@rossillo.net>
2022-03-09 18:26:16 -0500

    Zookeeper docs

---
+++ kontainers/zookeeper/MODULES.md
@@ -0,0 +1,15 @@
+# Module Zookeeper Kontainer
+
+Kontainer support for Apache [Zookeeper](https://zookeeper.apache.org/).
+
+## Usage
+
+```kotlin
+dependencies {
+    implementation("io.microkt.kontainers:kontainers-zookeeper:$version")
+}
+```
+
+# Package io.microkt.kontainers.zookeeper
+
+Provides a Zookeeper Kontainer, factory, and Kontainer specification.
--- kontainers/zookeeper/src/main/kotlin/io/microkt/kontainers/zookeeper/ZookeeperKontainer.kt
+++ kontainers/zookeeper/src/main/kotlin/io/microkt/kontainers/zookeeper/ZookeeperKontainer.kt
@@ -4,13 +4,12 @@ import io.microkt.kontainers.domain.GenericTcpKontainer
 import io.microkt.kontainers.domain.Kontainer
 import io.microkt.kontainers.domain.KontainerSpec
 
+/**
+* Provides an [Apache Zookeeper](https://zookeeper.apache.org/) [Kontainer].
+ *
+ * @author Scott Rossillo
+*/
 class ZookeeperKontainer(
     override val kontainerSpec: KontainerSpec,
     parent: Kontainer
-) : GenericTcpKontainer(kontainerSpec, parent) {
-    companion object Defaults {
-        const val ZOOKEEPER_IMAGE = "zookeeper"
-        const val ZOOKEEPER_VERSION = "3.6.3"
-        const val PORT: Int = 2181
-    }
-}
+) : GenericTcpKontainer(kontainerSpec, parent)
